adb21bb6141397178fdd66eb8c886043758e4807
qaqRose <singleqaq@gmail.com>
2018-10-17 23:06:45 +0800

    简单登录验证

---
+++ src/main/java/com/qxq/springsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.qxq.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * @author: QXQ
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()  // 不需要任何权限
+                .anyRequest().authenticated()  //All other paths must be authenticated
+                .and()
+                .formLogin()
+                .loginPage("/login")   //设置登录页面
+                .permitAll()
+                .and()
+                .logout()               // 登出
+                .permitAll();
+    }
+    
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user =
+                User.builder()    //创建一个USER角色的用户
+                        .username("user")
+                        .password("password")
+                        .passwordEncoder(encoder::encode)
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);    // 存储在内存中
+    }
+}
